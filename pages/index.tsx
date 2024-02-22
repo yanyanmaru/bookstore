bbd875b86eb36b471d1fbcf6dd04b3b82ef905ad
yanyanmaru <manmarutech@gmail.com>
2024-02-23 01:23:34 +0900

    データベース上のデータを取得して表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Inter } from "next/font/google";
-import { prisma } from "@/lib/prismaClient";
+import { Post, prisma } from "@/lib/prismaClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const [posts, setPosts] = useState<Post[]>([]);
   useEffect(() => {
     fetch('/api/posts')
       .then((res) => res.json())
-      .then(console.log);
+      .then((data) => setPosts(data));
   }, []);
 
   const handleClick = async () => {
-    await fetch('/api/posts', {
-      method: 'POST',
-    });
+    // await fetch('/api/posts', {
+    //   method: 'POST',
+    // });
+    await fetch('/api/posts')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+
   };
 
   return (
@@ -22,6 +28,11 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>fafafa</div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <p>{post.title}</p>
+        </div>
+      ))}
       <button onClick={handleClick}>post</button>
     </main>
   );
